00891bb5dedc8e70a19d6573dab3d938bdf9cbd3
muhammed2020osman <muhammed2020osman@gmail.com>
2026-01-07 21:23:02 +0200

    refactor: simplify FCMService provision by importing ConfigModule and removing custom factory.

--- src/fcm/fcm.module.ts
+++ src/fcm/fcm.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FCMService } from './fcm.service';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TypeOrmModule],
-    providers: [
-        {
-            provide: FCMService,
-            useFactory: (configService: ConfigService, dataSource: any) => {
-                return new FCMService(configService, dataSource);
-            },
-            inject: [ConfigService, 'DATA_SOURCE'],
-        },
-    ],
+    imports: [ConfigModule, TypeOrmModule],
+    providers: [FCMService],
     exports: [FCMService],
 })
-export class FCMModule {}
+export class FCMModule { }
